66085d85cfa2f0ab1ed85744f623ad8044fdd802
+ <+>
2022-06-14 15:09:06 +0300

    commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const main = document.getElementById('main')
+document.body.removeChild (main)
+
+const newHeader = document.createElement (h1)
+newHeader.id ='victory'
+newHeader.innerHTML = 'YOUR-NAME is the champion'
+document.body.appendChild (newHeader)
